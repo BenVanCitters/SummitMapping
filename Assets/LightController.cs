0957fe5a267fa2b80b5cca82a0d71cdcae228238
Ben Van Citters <van.citters.b@gmail.com>
2015-08-18 00:38:54 -0700

    initial commit

---
+++ Assets/LightController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightController : MonoBehaviour {
+
+	Light myLight;
+	// Use this for initialization
+	void Start () {
+		myLight = gameObject.GetComponent<Light> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		myLight.transform.Rotate (new Vector3 (0.1f, 0f, 0f));
+	}
+}
---
+++ Assets/MoviePlayer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoviePlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		Projector p = gameObject.GetComponent<Projector> ();
+//		Material material = gameObject.GetComponent<Material> ();
+		if (p != null) 
+		{
+			MovieTexture m = p.material.GetTexture("_ShadowTex") as MovieTexture; //mainTexture as MovieTexture;
+			m.loop = true;
+			m.Play();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
